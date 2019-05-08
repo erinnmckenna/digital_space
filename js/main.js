c12e1f4a80c4870e82ca2c8d82a4cff64a531445
Erinn <erinn.mckenna@yahoo.com>
2019-05-08 15:47:05 -0400

    added everything

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+function scrollWin() {
+  window.scrollTo({ left: 16900, top: 600 , behavior: 'smooth' });
+}
+
+function scrollWin2() {
+  window.scrollTo({ left: 10000, top: 9500 , behavior: 'smooth' });
+}
+
+function scrollWin3() {
+  window.scrollTo({ left: 2300, top: 17000 , behavior: 'smooth' });
+}
+
+function scrollWin4() {
+  window.scrollTo({ left: 0, top: 0 , behavior: 'smooth' });
+}
